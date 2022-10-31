141b72506c2da877a91d1b060914c9c10d042d59
Theo Morra <theo.morra@chase.net.nz>
2022-10-31 23:19:25 +1300

    Set the correct cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_cache_headers
-  private
+  before_action :set_default_cookies
+
   def set_cache_headers
     response.headers["Cache-Control"] = "no-cache, no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
+  end
+
+  def set_default_cookies
+    session[:uid] = true
+    cookies[:Tag] ||= 'default'
   end
 end
